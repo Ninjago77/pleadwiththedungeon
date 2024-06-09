0a6b2412acaf21fc59c4b5b9f6e61b0021540888
Ninjago77 <shanvantharunmozhi@gmail.com>
2024-06-09 14:53:59 -0700

    closing up

---
+++ README.md
@@ -0,0 +1,5 @@
+# Plead With The Dungeon
+A mini dungeon crawler about racing against the clock!
+### Credits:-
+2d Pixel Dungeon Asset Pack - Pixel_Poem (itch.io)
+Game by me:- ninjago77
--- main.ts
+++ main.ts
@@ -4,6 +4,23 @@ import "kaboom/global"
 // initialize context
 kaboom({ font: "monospace" });
 var selected_player = 0;
+var character_select = false;
+var points = 0;
+var since_time:number = 0;
+var timerId:any;
+const SPEED = 0.1;
+const FLOOR_TORCHES = false;
+
+const SCREEN_WIDTH = (16)*16;// 1525;
+const SCREEN_HEIGHT = (8)*16;// 700;
+
+const w = width() / SCREEN_WIDTH;
+const h = height() / SCREEN_HEIGHT;
+function autoResize(color:any=BLACK) {
+    setBackground(color);
+    camScale(new Vec2(w,h));
+    camPos(width() / 2, height() / 2);
+}
 loadRoot("https://raw.githubusercontent.com/Ninjago77/pleadwiththedungeon/main/");
 loadSprite("player_idle", "sprites/Players_Idle.png",{
     sliceX: 7*4,
@@ -70,21 +87,17 @@ loadSprite("thing_tileset", "sprites/Things_Tileset.png",{
 // loadSound("win","https://raw.githubusercontent.com/Ninjago77/pleadwiththedungeon/main/sounds/winner.mp3");
 // loadSound("wrong","https://raw.githubusercontent.com/Ninjago77/pleadwiththedungeon/main/sounds/wrong.mp3");
 
-const SCREEN_WIDTH = (16)*16;// 1525;
-const SCREEN_HEIGHT = (8)*16;// 700;
-
-const w = width() / SCREEN_WIDTH;
-const h = height() / SCREEN_HEIGHT;
-function autoResize(color:any=BLACK) {
-    setBackground(color);
-    camScale(new Vec2(w,h));
-    camPos(width() / 2, height() / 2);
+function posifyX(x:number) {
+    const offsetX = (width() - SCREEN_WIDTH) / 2;
+    return x + offsetX;
+}
+function posifyY(y:number) {
+    const offsetY = (height() - SCREEN_HEIGHT) / 2;
+    return y + offsetY;
 }
 
 function posify(x: number, y: number) {
-    const offsetX = (width() - SCREEN_WIDTH) / 2;
-    const offsetY = (height() - SCREEN_HEIGHT) / 2;
-    return pos(x + offsetX, y + offsetY);
+    return pos(posifyX(x),posifyY(y));
   }
 
 function random_choice(arr:Array<any>) {
@@ -102,89 +115,174 @@ function copyright() {
     ]);
 }
 
+function torchify(list_of_torches:number[][]){
+    // [top_torch_options,left_torch_options,bottom_torch_options,right_torch_options]
+    list_of_torches.forEach((e,i,a) => e.forEach(
+        (element,index,arr) => {
+        let d = add([
+            posify(element[0]*16,element[1]*16),
+            sprite("thing_tileset",{flipX:i==3}),
+        ]);
+        d.play(i % 2 == 0 ? "torch" : "side_torch",{loop:true});
+    }));
+}
+function thingify(thing:string,available_grounds:number[][],index:number) {
+    let d = add([
+        posify((available_grounds[index][0]+.5)*16,(available_grounds[index][1]+.5)*16),
+        sprite("thing_tileset"),
+        scale(thing == "peaks" ? (14/16) : 1),
+        anchor("center"),
+        area(),
+        thing,
+    ]);
+    d.play(thing, {loop:true});
+}
+
+var top_torch_options: any[] = [];
+var bottom_torch_options: any[] = [];
+var left_torch_options: any[] = [];
+var right_torch_options: any[] = [];
+var available_grounds: any[] = [];
+const wallCFG = {
+    "↖": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite("tile0")],
+    "A": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite(random_choice(["tile1","tile2","tile3","tile4"]))],
+    "↗": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite("tile5")],
+    "[": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite(random_choice(["tile10","tile20","tile30"]))],
+    "#": (p) => {
+        available_grounds.push([p.x,p.y]);
+        return ["ground",posify(p.x,p.y),sprite(random_choice(["tile6","tile7","tile8","tile9","tile16","tile17","tile18","tile19","tile26","tile27","tile28","tile29"]))];
+    },
+    "]": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite(random_choice(["tile15","tile25","tile35"]))],
+    "↙": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite("tile40")],
+    "V": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite(random_choice(["tile41","tile42","tile43","tile44"]))],
+    "↘": (p) => ["wall",area(),body({isStatic:true}),posify(p.x,p.y),sprite("tile45")],
+
+    "◤": (p) => {
+        // if (p.x % 2 == 0 && FLOOR_TORCHES) {top_torch_options.push([p.x,p.y])};
+        if (p.y % 2 == 0) {left_torch_options.push([p.x,p.y]);} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite("tile11")];
+    },
+    "^": (p) => {
+        if (p.x % 2 == 0 && FLOOR_TORCHES) {top_torch_options.push([p.x,p.y])} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite(random_choice(["tile12","tile13"]))];
+    },
+    "◥": (p) => {
+        // if (p.x % 2 == 0 && FLOOR_TORCHES) {top_torch_options.push([p.x,p.y])} else {available_grounds.push([p.x,p.y]);}
+        if (p.y % 2 == 0) {right_torch_options.push([p.x,p.y]);} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite("tile14")];
+    },
+    "(": (p) => {
+        if (p.y % 2 == 0) {left_torch_options.push([p.x,p.y]);} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite("tile21")];
+    },
+    ")": (p) => {
+        if (p.y % 2 == 0) {right_torch_options.push([p.x,p.y]);} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite("tile24")];
+    },
+    "◣": (p) => {
+        // if (p.x % 2 == 0 && FLOOR_TORCHES) {bottom_torch_options.push([p.x,p.y])} else {available_grounds.push([p.x,p.y]);}
+        if (p.y % 2 == 0) {left_torch_options.push([p.x,p.y]);} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite("tile31")];
+    },
+    "v": (p) => {
+        if (p.x % 2 == 0 && FLOOR_TORCHES) {bottom_torch_options.push([p.x,p.y])} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite(random_choice(["tile32","tile33"]))];
+    },
+    "◢": (p) => {
+        // if (p.x % 2 == 0 && FLOOR_TORCHES) {bottom_torch_options.push([p.x,p.y])} else {available_grounds.push([p.x,p.y]);}
+        if (p.y % 2 == 0) {right_torch_options.push([p.x,p.y]);} else {available_grounds.push([p.x,p.y]);}
+        return ["ground",posify(p.x,p.y),sprite("tile34")];
+    },
+
+    ".": (p) => ["nothing",posify(p.x,p.y),sprite("tile78")],
+};
 
 scene("character_select", () => {
+    character_select = true;
     autoResize(color(61,37,59));
 
     add([
-        posify(SCREEN_WIDTH/2, SCREEN_HEIGHT/3),
+        posify(SCREEN_WIDTH/2, (SCREEN_HEIGHT*(1.5/8))),
+        text("Plead With The Dungeon...", {
+            size: 12,
+            font: "monospace",
+        }),
+        anchor("bot"),
+        "charS"
+    ]);
+    add([
+        posify(SCREEN_WIDTH/2, (SCREEN_HEIGHT*(3/8))),
         text("Select Your Character", {
             size: 8,
             // font: "sans-serif",
         }),
+        anchor("bot"),
+        "charS"
+    ]);
+    add([
+        posify(SCREEN_WIDTH/2, (SCREEN_HEIGHT*(7/16))),
+        text("[->] [<-] & [Enter]", {
+            size: 8,
+            // font: "sans-serif",
+        }),
         anchor("center"),
+        "charS"
     ]);
     player_list.forEach((value,index,arr) => {
         add([
-            posify((SCREEN_WIDTH/2)-((arr.length/2)*16)+(index*16),SCREEN_HEIGHT/2),
-            sprite("player_selected"),
-            anchor("center"),
+            posify((SCREEN_WIDTH/2)-((arr.length/2)*16)+(index*16),(SCREEN_HEIGHT*(2/3))),
+            sprite("player_idle"),
+            anchor("topleft"),
+            "charS"
         ]).play(value,{loop:true});
+    });
+    var plys:GameObj[] = [];
+    player_list.forEach((value,index,arr) => {
+        let d = add([
+            posify((SCREEN_WIDTH/2)-((arr.length/2)*16)+(index*16),(SCREEN_HEIGHT*(2/3))),
+            sprite("player_selected"),
+            anchor("topleft"),
+            "charS"
+        ]);
+        d.play(value,{loop:true});
+        d.hidden = true;
+        d.onUpdate(() => {
+            d.hidden = (selected_player != index);
+        });
+        plys.push(d);
+    });
+    onKeyPress((key) => {
+        if (character_select) {
+            if (key == "left" || key == "a") {
+                if (selected_player == 0) {
+                    selected_player = player_list.length-1;
+                } else {
+                    selected_player--;
+                }
+            }
+            if (key == "right" || key == "d" || key == "space") {
+                if (selected_player == player_list.length-1) {
+                    selected_player = 0;
+                } else {
+                    selected_player++;
+                }
+            }
+            if (key == "enter") {
+                character_select = false;
+                destroyAll("charS");
+                go("level0");
+            }
+
+        }
     });
     
 });
-
 go("character_select");
 
-// var wall: any[] = ["wall"];
-// var ground: any[] = ["ground"];
-// var nothing: any[] = ["nothing"];
-// var top_torch_options: any[] = [];
-// var bottom_torch_options: any[] = [];
-// var left_torch_options: any[] = [];
-// var right_torch_options: any[] = [];
-// const wallCFG = {
-//     "↖": (p) => wall.concat(posify(p.x,p.y),sprite("tile0")),
-//     "A": (p) => wall.concat(posify(p.x,p.y),sprite(random_choice(["tile1","tile2","tile3","tile4"]))),
-//     "↗": (p) => wall.concat(posify(p.x,p.y),sprite("tile5")),
-//     "[": (p) => wall.concat(posify(p.x,p.y),sprite(random_choice(["tile10","tile20","tile30"]))),
-//     "#": (p) => {
-//         return ground.concat(posify(p.x,p.y),sprite(random_choice(["tile6","tile7","tile8","tile9","tile16","tile17","tile18","tile19","tile26","tile27","tile28","tile29"])));
-//     },
-//     "]": (p) => wall.concat(posify(p.x,p.y),sprite(random_choice(["tile15","tile25","tile35"]))),
-//     "↙": (p) => wall.concat(posify(p.x,p.y),sprite("tile40")),
-//     "V": (p) => wall.concat(posify(p.x,p.y),sprite(random_choice(["tile41","tile42","tile43","tile44"]))),
-//     "↘": (p) => wall.concat(posify(p.x,p.y),sprite("tile45")),
 
-//     "◤": (p) => {
-//         // if (p.x % 2 == 0) {top_torch_options.push([p.x,p.y])};
-//         if (p.y % 2 == 0) {left_torch_options.push([p.x,p.y]);}
-//         return ground.concat(posify(p.x,p.y),sprite("tile11"));
-//     },
-//     "^": (p) => {
-//         if (p.x % 2 == 0) {top_torch_options.push([p.x,p.y])};
-//         return ground.concat(posify(p.x,p.y),sprite(random_choice(["tile12","tile13"])));
-//     },
-//     "◥": (p) => {
-//         // if (p.x % 2 == 0) {top_torch_options.push([p.x,p.y])};
-//         if (p.y % 2 == 0) {right_torch_options.push([p.x,p.y]);}
-//         return ground.concat(posify(p.x,p.y),sprite("tile14"));
-//     },
-//     "(": (p) => {
-//         if (p.y % 2 == 0) {left_torch_options.push([p.x,p.y]);}
-//         return ground.concat(posify(p.x,p.y),sprite("tile21"));
-//     },
-//     ")": (p) => {
-//         if (p.y % 2 == 0) {right_torch_options.push([p.x,p.y]);}
-//         return ground.concat(posify(p.x,p.y),sprite("tile24"));
-//     },
-//     "◣": (p) => {
-//         // if (p.x % 2 == 0) {bottom_torch_options.push([p.x,p.y])};
-//         if (p.y % 2 == 0) {left_torch_options.push([p.x,p.y]);}
-//         return ground.concat(posify(p.x,p.y),sprite("tile31"));
-//     },
-//     "v": (p) => {
-//         if (p.x % 2 == 0) {bottom_torch_options.push([p.x,p.y])};
-//         return ground.concat(posify(p.x,p.y),sprite(random_choice(["tile32","tile33"])));
-//     },
-//     "◢": (p) => {
-//         // if (p.x % 2 == 0) {bottom_torch_options.push([p.x,p.y])};
-//         if (p.y % 2 == 0) {right_torch_options.push([p.x,p.y]);}
-//         return ground.concat(posify(p.x,p.y),sprite("tile34"));
-//     },
+// autoResize();
 
-//     ".": (p) => nothing.concat(posify(p.x,p.y),sprite("tile78")),
-// };
+
 // const level1Maps = {
 //     "leftleft": [
 //     "................",
@@ -228,63 +326,163 @@ go("character_select");
 //     ],
 // }
 
+scene("level0", () => {
+    autoResize();
+    timerId = setInterval(()=>{since_time++;},1000);
+    addLevel([
+        "................",
+        ".↖AAAAA↗↖AAAAA↗.",
+        ".[◤^^^◥][◤^^^◥].",
+        ".[(#vv)AA(vv#)].",
+        ".[()AA(^^)AA()].",
+        ".[◣◢][◣vv◢][◣◢].",
+        ".↙VV↘↙VVVV↘↙VV↘.",
+        "................",
+    ],{
+        tileHeight: 16-1,
+        tileWidth: 16-1,
+        tiles: wallCFG,
+        
+    });
+    
+    [
+       ["peaks",0],
+       ["peaks",1],
+       ["peaks",2],
+       ["peaks",3],
+       ["peaks",4],
+       ["peaks",5],
+       ["coin",20],
+       ["coin",23],
+       ["coin",18],
+       ["coin",25],
+
+    ].forEach((e,i,a) => thingify(e[0].toString(),available_grounds,Number(e[1])));
+    const p = add([
+        posify(SCREEN_WIDTH-3,SCREEN_HEIGHT-1.5),
+        text(
+            points.toString(),
+            {size: 8,}
+        ),
+        anchor("botright"),
+        "points",
+    ]);
+    add([
+        posify(SCREEN_WIDTH-16,SCREEN_HEIGHT-1),
+        sprite("tile86"),
+        anchor("bot"),
+        
+    ]);
+    onCollide("peaks","player",() => {
+        go("death");
+    }); 
+    onCollide("coin","player",(a,b) => {
+        destroy(a);
+        points++;
+        p.text = points.toString();
+        if (points >= 4) {
+            go("win");
+        }
+    }); 
+
+    const player = add([
+        sprite("player_idle", {
+            animSpeed: 1,
+            frame: 4,
+        }),
+        posify(SCREEN_WIDTH/2,SCREEN_HEIGHT*(2/3)),
+        anchor("center"),
+        scale(1),
+        area(),
+        body({ isStatic: false}),
+        "player",
+    ]);
+    player.play(player_list[selected_player],{loop:true});
+    onKeyDown((key) => {
+        let Xvel = 0;
+        let Yvel = 0;
+        if (key == "w" || key == "up") {
+            Yvel += -SPEED*w;
+        }
+        if (key == "s" || key == "down") {
+            Yvel += SPEED*w;
+        }
+        if (key == "a" || key == "left") {
+            Xvel += -SPEED*h;
+        }
+        if (key == "d" || key == "right") {
+            Xvel += SPEED*h;
+        }
+        if (Xvel != 0) {
+            player.flipX = Xvel < 0;
+        }
+        player.moveBy(Xvel,Yvel);
+        // move_history = key;
+    })
+    
+
+    torchify([top_torch_options,left_torch_options,bottom_torch_options,right_torch_options]);
+
+})
+
 // const level1 = addLevel(
-// // [
-// // "↖AAA↗...........",
-// // "[◤^◥]...........",
-// // "[(#)]............",
-// // "[◣v◢]...........",
-// // "↙VVV↘...........",
-// // "................",
-// // "................",
-// // "................",
-// // ]
-// level1Maps["bottombottom"]
+// [
+// "↖AAA↗...........",
+// "[◤^◥]...........",
+// "[(#)]...........",
+// "[◣v◢]...........",
+// "↙VVV↘...........",
+// "................",
+// "................",
+// "................",
+// ]
+// // level1Maps["bottombottom"]
 // ,{
 //     tileHeight: 16-1,
 //     tileWidth: 16-1,
 //     tiles: wallCFG,
 
 // })
-// top_torch_options.forEach((element,index,arr) => {
-//     let d = add([
-//         posify(element[0]*16,element[1]*16),
-//         sprite("thing_tileset"),
-//     ]);
-//     d.play("torch",{loop:true});
-// });
-// left_torch_options.forEach((element,index,arr) => {
-//     let d = add([
-//         posify(element[0]*16,element[1]*16),
-//         sprite("thing_tileset"),
-//     ]);
-//     d.play("side_torch",{loop:true});
-// });
-// bottom_torch_options.forEach((element,index,arr) => {
-//     let d = add([
-//         posify(element[0]*16,element[1]*16),
-//         sprite("thing_tileset",{flipY:true}),
-//     ]);
-//     d.play("torch",{loop:true});
-// });
-// right_torch_options.forEach((element,index,arr) => {
-//     let d = add([
-//         posify(element[0]*16,element[1]*16),
-//         sprite("thing_tileset",{flipX:true}),
-//     ]);
-//     d.play("side_torch",{loop:true});
-// });
 
-const player = add([
-    sprite("player_idle", {
-      animSpeed: 1,
-      frame: 4,
-    }),
-    posify(SCREEN_WIDTH/2,SCREEN_HEIGHT/2),
-    anchor("center"),
-    scale(1),
-]);
-player.onMousePress((m) => selected_player++);
 
-player.play(player_list[selected_player],{loop:true});
-copyright();
+// const player = add([
+//     sprite("player_idle", {
+//       animSpeed: 1,
+//       frame: 4,
+//     }),
+//     posify(SCREEN_WIDTH/2,SCREEN_HEIGHT/2),
+//     anchor("center"),
+//     scale(1),
+// ]);
+// player.onMousePress((m) => selected_player++);
+
+// player.play(player_list[selected_player],{loop:true});
+// copyright();
+scene("death",() => {
+    autoResize();
+    add([
+        posify(SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
+        text(`
+       Oh no!
+     You died :( 
+Reload to try again...`, {
+            size: 12,
+            // font: "sans-serif",
+        }),
+        anchor("center"),
+        "charS"
+    ]);
+});
+scene("win",() => {
+    autoResize();
+    add([
+        posify(SCREEN_WIDTH/2, SCREEN_HEIGHT/2),
+        text(`You won! in ${since_time}s`, {
+            size: 12,
+            // font: "sans-serif",
+        }),
+        anchor("center"),
+        "charS"
+    ]);
+    clearInterval(timerId);
+});
